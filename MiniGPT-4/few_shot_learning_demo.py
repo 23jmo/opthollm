70fbc782b14e040afaa1a9a9c25b21309de2a3da
“23jmo” <2023johnathanmo@gmail.com>
2023-07-06 21:47:25 -0700

    show uploaded images in chat

--- MiniGPT-4/few_shot_learning_demo.py
+++ MiniGPT-4/few_shot_learning_demo.py
@@ -98,19 +98,21 @@ def gradio_reset():
         []
 
 
-def upload_img(gr_img, chat_state, img_list, img_emb_list):
+def upload_img(gr_img, chat_state, img_list, img_emb_list, chatbot):
     if gr_img is None:
         return None, None, gr.update(interactive=True), chat_state, img_list, img_emb_list
     img_list.append(gr_img)
     # upload an image to the chat
     chat.upload_img(gr_img, chat_state, img_emb_list)
     # update image, text_input, upload_button, chat_state, gallery, img_emb_list
+    chatbot = chatbot + [((gr_img, None),None)]
     return gr.update(value=None, interactive=False), \
         gr.update(interactive=True, placeholder='Type and press Enter'), \
         gr.update(value="Send more images after sending a message", interactive=False), \
         chat_state, \
         img_list, \
-        img_emb_list
+        img_emb_list, \
+        chatbot
 
 def gradio_ask(user_message, chatbot, chat_state):
     if len(user_message) == 0:
@@ -221,8 +223,8 @@ with gr.Blocks() as demo:
 
    
 
-    upload_button.click(upload_img, [image, chat_state, img_list, img_emb_list],
-                        [image, text_input, upload_button, chat_state, gallery, img_emb_list])
+    upload_button.click(upload_img, [image, chat_state, img_list, img_emb_list, chatbot],
+                        [image, text_input, upload_button, chat_state, gallery, img_emb_list, chatbot])
 
     few_shot_learning_button\
         .click(gradio_FSL, [chatbot, chat_state, image, img_list, img_emb_list],
